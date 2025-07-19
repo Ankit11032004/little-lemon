2e2ff57f7dd5368b8df2b01739050241e53339b6
Ankit11032004 <159641091+Ankit11032004@users.noreply.github.com>
2025-07-19 20:37:55 +0530

    addAPI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import LoginPage from "./pages/login";
 import SignupPage from "./pages/Signup";
 import HomePage from "./pages/HomePage.jsx";
+import Menu from "./components/Menu";
 import "./styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/" element={<HomePage />} />
+                  <Route path="/menu" element={<Menu />} />
         </Routes>
       </Router>
     </div>
